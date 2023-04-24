8cd68f159fc3e2385c28e6a667205ad0d42fc8e1
Chris <cgolayac@correo.udistrital.edu.co>
2023-04-24 16:15:17 -0500

    update admin view

--- api/admin.py
+++ api/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Office
 from .models import Employee
 
-admin.site.register(Office)
-admin.site.register(Employee)
+@admin.register(Office)
+class officeAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'city')
+    list_filter = ['city',]
+    
+@admin.register(Employee)
+class employeeAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'office')
+    list_filter = ['office',]
