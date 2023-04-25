af4711b76122f6d21c2d5440cc25bdcf56fb0d15
Chris <cgolayac@correo.udistrital.edu.co>
2023-04-25 13:38:30 -0500

    fixed views

--- api/views/employee_views.py
+++ api/views/employee_views.py
@@ -14,37 +14,38 @@ class EmployeeDetailView(DetailView):
     context_object_name = 'employee'
 
 def Employee_list(request):
-    Employees = Employee.objects.all()
-    return render(request, 'Employee_list.html', {'Employees': Employees})
+    employees = Employee.objects.all()
+    return render(request, 'Employee_list.html', {'employees': employees})
 
 def Employee_detail(request, pk):
-    Employee = get_object_or_404(Employee, pk=pk)
-    return render(request, 'Employee_detail.html', {'Employee': Employee})
+    employee = get_object_or_404(Employee, pk=pk)
+    return render(request, 'Employee_detail.html', {'employee': employee})
 
 def Employee_create(request):
+    print(request.POST)
     if request.method == 'POST':
         form = EmployeeForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('Employee_list')
+            return redirect('employee_list')
     else:
         form = EmployeeForm()
-    return render(request, 'Employee_form.html', {'form': form})
+    return render(request, 'employee_form.html', {'form': form})
 
 def Employee_update(request, pk):
-    Employee = get_object_or_404(Employee, pk=pk)
+    employee = get_object_or_404(Employee, pk=pk)
     if request.method == 'POST':
-        form = EmployeeForm(request.POST, instance=Employee)
+        form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
             form.save()
-            return redirect('Employee_list')
+            return redirect('employee_list')
     else:
         form = EmployeeForm(instance=Employee)
     return render(request, 'Employee_form.html', {'form': form})
 
 def Employee_delete(request, pk):
-    Employee = get_object_or_404(Employee, pk=pk)
+    employee = get_object_or_404(Employee, pk=pk)
     if request.method == 'POST':
-        Employee.delete()
-        return redirect('Employee_list')
-    return render(request, 'Employee_confirm_delete.html', {'Employee': Employee})
+        employee.delete()
+        return redirect('employee_list')
+    return render(request, 'employee_confirm_delete.html', {'employee': employee})
